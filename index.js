b40c97eeada420f1b0e9cb37da58fece6b2d0c2b
Dhananjay Puranik <dhananjaypuranik8@gmail.com>
2024-09-06 16:20:54 +0530

    basic setup

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello Team");
-});
-
 app.listen(5050, () => {
   console.log("Server started !");
 });
